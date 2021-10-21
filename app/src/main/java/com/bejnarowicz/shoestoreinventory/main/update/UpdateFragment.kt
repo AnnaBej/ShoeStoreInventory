9cd3e322af5370b7baf353a62fb26157ff04da7e
Anna Bejnarowicz <annbejmail@gmail.com>
2021-10-21 10:29:29 +0200

    initial

--- app/src/main/java/com/bejnarowicz/shoestoreinventory/main/update/UpdateFragment.kt
+++ app/src/main/java/com/bejnarowicz/shoestoreinventory/main/update/UpdateFragment.kt
@@ -1,9 +1,9 @@
 package com.bejnarowicz.shoestoreinventory.main.update
 
+import android.app.Activity
 import android.app.AlertDialog
-import android.net.Uri
+import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.*
 import android.widget.SeekBar
 import androidx.fragment.app.Fragment
@@ -126,5 +126,23 @@ class UpdateFragment : Fragment() {
         builder.setMessage("Your inputted data will be lost")
         builder.create().show()
     }
+
+    fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == GALLERY_REQUEST_CODE) {
+            binding.ivPhoto.setImageURI(data?.data)
+        }
+    }
+
+    companion object {
+        private const val GALLERY_REQUEST_CODE = 3
+    }
+
 
 }
